d2736ac5ba0acf2bb3c2dc5eab3cb2a6395a2f70
jmacboy <j.m.alvarez.camacho@gmail.com>
2025-06-18 12:13:34 -0400

    updated with functions and notifications

--- nube-functions/functions/src/index.ts
+++ nube-functions/functions/src/index.ts
@@ -9,11 +9,46 @@
 
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
-
+import * as admin from "firebase-admin";
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
+admin.initializeApp();
 
 export const helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+export const sendNewMessageNotification = onRequest(
+  async (request, response) => {
+    if (request.method !== "POST") {
+      response.status(405).send("Method Not Allowed");
+      return;
+    }
+    const {title, body, token} = request.body;
+    if (!title || !body || !token) {
+      response.status(400).send("Bad Request: Missing title, body, or token");
+      return;
+    }
+    const payload = {
+      token:
+        token,
+      notification: {
+        title: title,
+        body: body,
+      },
+    };
+
+    const res = await admin.messaging().send(payload);
+    if (!res) {
+      response
+        .status(500)
+        .send("Internal Server Error: Failed to send message");
+      return;
+    }
+    logger.info("Sending message:", payload);
+    logger.info("Message sent successfully:", res);
+    response.status(200).send({
+      success: true,
+    });
+  }
+);
